e7bcb14571e09347528565c445743e2d20eea9f9
blenda.guedes <blenda.guedes@fastsolucoes.com.br>
2019-10-29 04:35:18 -0300

    Commiting the last 4th tutorial part. First I was using 'id' to identify the object, but since the app uses a pattern thar demands 'pk', it was changed.

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
-# Create your models here.
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -12,6 +12,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
